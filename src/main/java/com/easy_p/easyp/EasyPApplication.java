91b98b47b38ecbd9439bade7715a01542ae77baf
minsik2434 <minsik2434@gmail.com>
2025-01-20 12:33:51 +0900

    project init

---
+++ src/main/java/com/easy_p/easyp/EasyPApplication.java
@@ -0,0 +1,13 @@
+package com.easy_p.easyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyPApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easy_p/easyp/EasyPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy_p.easyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
